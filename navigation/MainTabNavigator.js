ccef436684486f9e45d8f15eb42399f5e6800d5a
diegodlc <39403988+diegodlc@users.noreply.github.com>
2020-03-03 23:54:22 +0100

    Subida de ficheros
    Faltan node modules y web-build

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,88 @@
+import { Icon } from 'expo'
+import React from 'react'
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation'
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import MessagesScreen from '../screens/MessagesScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+import TopPicksScreen from '../screens/TopPicksScreen'
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    headerMode: 'none',
+  },
+)
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      Icon={Icon.MaterialCommunityIcons}
+      focused={focused}
+      name="fire"
+    />
+  ),
+}
+
+const TopPicksStack = createStackNavigator(
+  {
+    TopPicks: TopPicksScreen,
+  },
+  {
+    headerMode: 'none',
+  },
+)
+
+TopPicksStack.navigationOptions = {
+  tabBarLabel: 'Explora',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="diamond" />
+  ),
+}
+
+/*
+const MessagesStack = createStackNavigator(
+  {
+    Messages: MessagesScreen,
+  },
+  {
+    headerMode: 'none',
+  },
+)
+
+MessagesStack.navigationOptions = {
+  tabBarLabel: '¿?',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="commenting-o" />
+  ),
+}
+*/
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: ProfileScreen,
+  },
+  {
+    headerMode: 'none',
+  },
+)
+
+ProfileStack.navigationOptions = {
+  tabBarLabel: 'Perfil',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon Icon={Icon.Feather} focused={focused} name="user" />
+  ),
+}
+
+export default createBottomTabNavigator({
+  HomeStack,
+  TopPicksStack,
+  //MessagesStack,
+  ProfileStack,
+})
