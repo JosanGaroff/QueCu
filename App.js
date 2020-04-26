0d5ced56479021dd6aeb2156de0b750827f64d4f
deelac <d.delacal@alumnos.upm.es>
2020-04-26 12:13:26 +0200

    fetch BBDD

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { AppLoading, Asset, Font, Icon } from 'expo'
 import React from 'react'
-import { StatusBar, StyleSheet, View } from 'react-native'
+import { StatusBar, StyleSheet, View, Text, Button } from 'react-native'
 import AppNavigator from './navigation/AppNavigator'
+
+localhost = '192.168.1.43';  // PONER CADA UNO LA DIRECCIÓN IP DE SU ORDENADOR
+
 
 export default class App extends React.Component {
   state = {
+    data: [],
+    isLoading: true,
     isLoadingComplete: false,
+  }
+
+  hacerLogin = () =>{
+    console.log("has pulsado")
+    let url = new URL('http://'+localhost+':8080/ISST-20-TFG/FormLoginServlet')
+    url.search = new URLSearchParams({
+        email:'d',
+        password:''
+    })
+    fetch('http://'+localhost+':8080/ISST-20-TFG/FormLoginServlet', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            
+          }
+        })
+  }
+
+   componentDidMount() { 
+
+    fetch('http://'+localhost+':8080/ISST-20-TFG/FormPeticionJson', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+          }
+        })
+    .then((response) => response.json())
+        .then((json) => {
+          this.setState({ data: json });
+          console.log(this.state.data);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+
   }
 
   render() {
@@ -20,6 +63,14 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
+        <Text >
+        {String(JSON.stringify(this.state.data))}
+        </Text>
+
+        <Button onPress={this.hacerLogin} title="Pulsa para login" />
+
+
+
           <StatusBar hidden />
           <AppNavigator />
         </View>
@@ -52,6 +103,8 @@ export default class App extends React.Component {
   _handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true })
   }
+
+
 }
 
 const styles = StyleSheet.create({
